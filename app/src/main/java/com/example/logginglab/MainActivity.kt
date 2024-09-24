35e4ba721953e82c06372933d22559e3fdb36d95
Hamjoshua <biryukov-10@list.ru>
2024-09-24 12:59:44 +0700

    Задание 1

--- app/src/main/java/com/example/logginglab/MainActivity.kt
+++ app/src/main/java/com/example/logginglab/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.logginglab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.button_ok);
+        btn.setOnClickListener {
+            showToast();
+        }
+
+    }
+
+    private fun showToast(){
+        Toast.makeText(this, "Кнопка ОК", Toast.LENGTH_SHORT).show();
     }
 }
